0075bd4f0ae5840523f39ba0a0b28abda72ced8e
Alexander <band.kist@gmail.com>
2020-07-31 15:45:09 +0300

    upg js (move head snake)

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const cell = 30;
 
 function drawBoard() {
     ctx.strokeRect(0,0,canvas.width,canvas.height);
-
     for (let i = 0; i <= 15; i++) {
         for (let h = 0; h <= 8; h++) {
             if (i % 2 !== 0) {
@@ -25,7 +24,7 @@ function drawBoard() {
         }
     }
 }
-drawBoard();
+
 
 const food = new Image();
 food.src = getUrlFood();
@@ -45,12 +44,61 @@ const foodCoord = {
     x: Math.floor(Math.random() * 15) * cell,
     y: Math.floor(Math.random() * 15) * cell
 }
+function drawFood() {
+    ctx.drawImage(food, foodCoord.x, foodCoord.y);
+}
 
 
 
-function drawFood () {
-    food.onload = function () {
-        ctx.drawImage(food, foodCoord.x, foodCoord.y);
+let snake = [{ x: 7 * cell, y: 7 * cell}];
+
+
+const snakeImgHead = new Image();
+snakeImgHead.src = "images/snake-head.png";
+snakeImgHead.onload = function () {
+    ctx.drawImage(snakeImgHead, snake[0].x, snake[0].y);
+}
+
+
+document.addEventListener("keydown", where)
+let whereMove;
+function where(event) {
+    if ((event.keyCode === 37 || event.keyCode === 65) && whereMove !== "right") {
+        whereMove = "left";
+    } else if ((event.keyCode === 38 || event.keyCode === 87) && whereMove !== "down") {
+        whereMove = "up";
+    }
+    if ((event.keyCode === 39 || event.keyCode === 68) && whereMove !== "left") {
+        whereMove = "right";
+    } else if ((event.keyCode === 40 || event.keyCode === 83) && whereMove !== "up") {
+        whereMove = "down";
     }
 }
-drawFood ();
+
+
+function drawGame() {
+
+    drawBoard();
+    drawFood();
+
+    let snakeHeadX = snake[0].x;
+    let snakeHeadY = snake[0].y;
+
+    if (whereMove === "left") {snakeHeadX -= cell}
+    if (whereMove === "right") {snakeHeadX += cell}
+    if (whereMove === "up") {snakeHeadY -= cell}
+    if (whereMove === "down") {snakeHeadY += cell}
+
+    snake.unshift({
+        x: snakeHeadX,
+        y: snakeHeadY
+    });
+
+    snake.pop();
+
+    ctx.drawImage(snakeImgHead, snake[0].x, snake[0].y);
+
+}
+
+let game = setInterval (drawGame, 200);
+
